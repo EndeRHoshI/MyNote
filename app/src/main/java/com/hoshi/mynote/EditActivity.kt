39b4bcb67b4f146a524bf3a7fa13d33ae44a12d3
lqx <kllqx@qq.com>
2020-07-17 11:14:57 +0800

    :tada: first commit

---
+++ app/src/main/java/com/hoshi/mynote/EditActivity.kt
@@ -0,0 +1,11 @@
+package com.hoshi.mynote
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+public class EditActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    super.onCreate(savedInstanceState, persistentState)
+  }
+}
